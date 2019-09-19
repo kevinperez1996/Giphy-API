b9a1974c1c95676870e4dfcd08a0ad0eb452bd49
Kevin Perez <kevinperez996@outlook.com>
2019-09-19 15:41:19 -0400

    added some styling to makes images stack next to one another. Also added part of the pause play logic, but need to work out some bugs.

--- giphy.js
+++ giphy.js
@@ -1,10 +1,10 @@
 
 var animals = ["Dog", "Cat", "Horse", "Monkey", "Elephant"];
 
-function makeButtons (){
+function makeButtons() {
     $("#btnDisplay").empty();
 
-    for (var i = 0; i < animals.length; i++){
+    for (var i = 0; i < animals.length; i++) {
 
         var btn = $("<button>");
         btn.addClass("gifButtons")
@@ -13,12 +13,11 @@ function makeButtons (){
 
         $("#btnDisplay").append(btn);
 
-
     };
 }
 makeButtons();
 
-$("#addbtn").on("click", function(event){
+$("#addbtn").on("click", function (event) {
     event.preventDefault();
 
     var newAnimal = $("#animalAdd").val().trim();
@@ -30,49 +29,47 @@ $("#addbtn").on("click", function(event){
 $(document).on("click", ".gifButtons", displayGif);
 
 function displayGif() {
+    $("#animalDisplay").empty();
     var animal = $(this).attr("gifName");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=QPbdWuyzSoqRRSGMjQ5XoBVCzfwgaI2A&q=" + animal + "&limit=10&offset=0&rating=G&lang=en";
-    //var state1 = "still"
-   // var state2 = "animated"    
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        
-        for (var j = 0; j < 9; j++) {
-            
+
+        for (var j = 0; j < 10; j++) {
+
             var gifDiv = $("<div class='animal'>");
             var rating = response.data[j].rating;
-            
+
             var t1 = $("<p>").text("Rating: " + rating);
             gifDiv.append(t1);
-            
-            var animated = response.data[j].images.fixed_height.url;
-            var still = response.data[j].images.fixed_height_still.url;
-            
-            var gifURL1 = still;
-            var gifURL2 = animated;
+
+            var gifURL1 = response.data[j].images.fixed_height_still.url;
+            var gifURL2 = response.data[j].images.fixed_height.url;
             var gif = $("<img class='actualGif' state='still'>").attr("src", gifURL1);
-            
-            $(document).on("click", ".actualGif", function(){
 
-                var state = $(".actualGif").attr("state");
 
-                if (state === "still"){
-                gif.attr("src", gifURL2);
-                $(".actualGif").attr("state", "animated");
+            $(document).on("click", ".actualGif", function () {
+
+                var state = $(".animal").attr("state");
+
+                if (state === "still") {
+                    $(this).attr("src", gifURL2);
+                    $(".actualGif").attr("state", "animated");
+                
                 }
                 else {
-                gif.attr("src", gifURL1);
-                $(".actualGif").attr("state", "still");
+                    $(this).attr("src", gifURL1);
+                    $(".actualGif").attr("state", "still");
+                   
                 }
+                
             });
 
             gifDiv.append(gif);
-            
-            
             $("#animalDisplay").prepend(gifDiv);
         }
     });
